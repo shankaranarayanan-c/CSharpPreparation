3bdde19d50dfc933bd822074e49b26a0a73fca45
sankara narayanan chandrasekar <shankaranarayanan.c@gmail.com>
2024-08-11 18:55:14 +0200

    chore: add inheritance example and updated readme

---
+++ 10_Inheritance/Program.cs
@@ -0,0 +1,99 @@
+﻿
+FooBase fooBase = new FooBase(10);
+FooDerived fooDerived = new FooDerived(20);
+fooBase.display();
+fooDerived.display();
+
+//Trying polymorphic behaviour
+FooBase fooBase1 = new FooDerived(40);
+fooBase1.display();                                                 // The fooBase1 calls the base class instead of derived class
+
+
+Polymorphic.FooBase fooBase2 = new Polymorphic.FooDerived(40);
+fooBase2.display();                                                 // virtual and override keywords enable polymorphic behaviour
+
+
+class FooBase
+{
+    int x;
+
+    public FooBase(int x){
+        this.x =x;
+    }
+
+    public void display(){
+        System.Console.WriteLine("Base Class x: {0}", this.x);
+    }
+}
+
+class FooDerived : FooBase
+{
+
+    int y;
+
+    public FooDerived(int y):base(y){
+        this.y = y;
+    }
+
+    public void display(){
+        System.Console.WriteLine("Derived Class x: {0}", this.y);
+    }
+}
+
+namespace Polymorphic{
+        class FooBase
+    {
+        int x;
+
+        public FooBase(int x){
+            this.x =x;
+        }
+
+        public virtual void display(){
+            System.Console.WriteLine("Base Class x: {0}", this.x);
+        }
+    }
+
+    class FooDerived : FooBase
+    {
+
+        int y;
+
+        public FooDerived(int y):base(y){
+            this.y = y;
+        }
+
+        public override void display(){
+            System.Console.WriteLine("Derived Class x: {0}", this.y);
+        }
+    }
+}
+
+namespace RestrictInheritance{
+        sealed class FooBase
+    {
+        int x;
+
+        public FooBase(int x){
+            this.x =x;
+        }
+
+        public void display(){
+            System.Console.WriteLine("Base Class x: {0}", this.x);
+        }
+    }
+
+    // class FooDerived : FooBase                                           //cant inherit a sealed class
+    // {
+
+    //     int y;
+
+    //     public FooDerived(int y):base(y){
+    //         this.y = y;
+    //     }
+
+    //     public override void display(){
+    //         System.Console.WriteLine("Derived Class x: {0}", this.y);
+    //     }
+    // }
+}
--- README.md
+++ README.md
@@ -6,12 +6,11 @@
 3. Install .Net SDK Ex: In Linux sudo snap install dotnet-sdk
 
 ##Create a new C#Project
-1. In termnial create a new folder ex: `mkdir 01_HelloWorld`
-2. `cd 01_HelloWorld`
-3. `dotnet new console --name 01_HelloWorld`
+1. `dotnet new console --name 01_HelloWorld`
 
 ##Build the C# Project
-1. `dotnet build -o out 01_HelloWorld.csproj`
+1. cd into the newly created project `cd 01_HelloWorld`
+2. `dotnet build -o out 01_HelloWorld.csproj`
 
 ##Execute the binary
 1. `./out/01_HelloWorld`
