8272a11b441d2e24af9f1b9d495cfe540eceea1b
sankara narayanan chandrasekar <shankaranarayanan.c@gmail.com>
2024-08-11 20:04:10 +0200

    chore: add interface example

---
+++ 11_Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿AutonomousDriving ad = new AutonomousDriving();
+ad.totalObjectsDetected("data");
+ObjDetectionAlgorithm.display();
+
+interface ObjDetectionAlgorithm{
+
+    // int totalSensors;                                        // cannot have instance fields
+
+    static int totalSensors;                                    // Interface cannot have instance fields only static and abstract methods
+
+    int totalObjectsDetected(string data);
+
+    static void display(){
+        System.Console.WriteLine("Interface Hello!");
+    }
+    
+}
+
+class AutonomousDriving : ObjDetectionAlgorithm{
+
+    static AutonomousDriving(){
+        ObjDetectionAlgorithm.totalSensors = 10;
+    }
+
+    public int totalObjectsDetected(string data){
+        System.Console.WriteLine("hi {0}", data);
+        return 0;
+    }
+}
